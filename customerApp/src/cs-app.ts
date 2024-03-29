49e41fcb62c4af1a23e2c487eaaf525334bf01d8
wb_lishizeng <wb_lishizeng@meituan.com>
2024-03-29 20:07:14 +0800

    p1

---
+++ customerApp/src/cs-app.ts
@@ -0,0 +1,127 @@
+import * as faye from 'faye';
+import { uniqueId } from 'lodash';
+const FAYE_URL = 'http://localhost:8000/faye';
+
+// Faye connection
+let client;
+
+/**
+ * Issue 'get-customers' request, return response from message broker
+ *
+ * @param customerId - id of customer to return
+ */
+async function getCustomers(customerId) {
+  // build Nest-shaped message
+  const payload = getPayload('/get-customers', { customerId }, uniqueId());
+
+  return new Promise<void>((resolve, reject) => {
+    // subscribe to the response message
+    const subscription = client.subscribe('/get-customers_res', (result) => {
+      console.log(
+        `==> Receiving 'get-customers' reply: \n${JSON.stringify(
+          result.response,
+          null,
+          2,
+        )}\n`,
+      );
+    });
+
+    // once response is subscribed, publish the request
+    subscription.then(() => {
+      console.log(
+        `<== Sending 'get-customers' request with payload:\n${JSON.stringify(
+          payload,
+        )}\n`,
+      );
+      const pub = client.publish('/get-customers_ack', payload);
+      pub.then(() => {
+        // wait .5 second to ensure subscription handler executes
+        // then unsubscribe and resolve
+        setTimeout(() => {
+          subscription.cancel();
+          resolve();
+        }, 500);
+      });
+    });
+  });
+}
+
+/**
+ * Issue 'add-customer' event
+ *
+ * @param name - name of customer to add
+ */
+async function addCustomer(name) {
+  const payload = getPayload('/add-customer', { name });
+  try {
+    await client.publish('/add-customer', payload);
+    console.log(
+      `<== Publishing add-customer event with payload:\n${JSON.stringify(
+        payload,
+      )}\n`,
+    );
+  } catch (error) {
+    console.log('Error publishing event: ', error);
+  }
+}
+
+function usage() {
+  console.log('Usage: node customer-app add <customer-name> | get [id]');
+  console.log('\t get [id]: send get-customers request and print response');
+  console.log(
+    '\t\t if id is passed, get matching customer by id, else get all\n',
+  );
+  console.log('\t add <customer-name> : send add-customer event');
+  process.exit(0);
+}
+
+/**
+ * Build Nest-shaped payload
+ *
+ * @param pattern string - message pattern
+ * @param value any - payload value
+ * @param id number - optional (used only for requests) message Id
+ */
+function getPayload(pattern, value, id?) {
+  let payload = {
+    pattern: pattern,
+    data: value,
+  };
+  if (id) {
+    payload = Object.assign(payload, { id });
+  }
+  return payload;
+}
+
+async function main() {
+  try {
+    client = new faye.Client(FAYE_URL);
+    console.log('Faye customer app starts...\n===========================');
+
+    // Call appropriate function based on cmd line arg
+    if (process.argv[2] === 'add') {
+      if (process.argv[3]) {
+        await addCustomer(process.argv[3]);
+      } else {
+        usage();
+      }
+    } else if (process.argv[2] === 'get') {
+      await getCustomers(process.argv[3]);
+    } else {
+      usage();
+    }
+
+    client.disconnect();
+    process.exit(0);
+  } catch (error) {
+    console.log('Error connecting to Faye: ', error);
+    process.exit(0);
+  }
+}
+
+// make sure we get a command argument on OS cmd line
+if (process.argv.length < 3) {
+  usage();
+}
+
+main();
---
+++ customerService/src/service.ts
@@ -0,0 +1,86 @@
+import * as faye from 'faye';
+
+const FAYE_URL = 'http://localhost:8000/faye';
+
+const customerList = [{ id: 1, name: 'Acme, Inc.' }];
+let lastId = customerList.length;
+
+// Faye connection
+let client;
+
+/**
+ * callback handler. This is registered for '/get-customers' topic.
+ *
+ * @param packet inbound request payload
+ */
+function getCustomers(packet): void {
+  const message = parsePacket(packet);
+  console.log(
+    `\n========== <<< 'get-customers' message >>> ==========\n${JSON.stringify(
+      message,
+    )}\n=============================================\n`,
+  );
+
+  // filter customers list if there's a `customerId` param
+  const customers =
+    message.data && message.data.customerId
+      ? customerList.filter(
+          (cust) => cust.id === parseInt(message.data.customerId, 10),
+        )
+      : customerList;
+
+  client.publish('/get-customers_res', getPayload({ customers }, message.id));
+}
+
+/**
+ * Callback handler. This is registered for '/add-customer' topic.
+ *
+ * @param packet inbound request payload
+ */
+function addCustomer(packet): void {
+  const message = parsePacket(packet);
+  console.log(
+    `\n========== <<< 'add-customer' message >>> ==========\n${JSON.stringify(
+      message,
+    )}\n=============================================\n`,
+  );
+
+  customerList.push({
+    id: lastId + 1,
+    name: message.data.name,
+  });
+  lastId++;
+}
+
+function getPayload(value, id) {
+  return {
+    err: null,
+    response: value,
+    isDisposed: true,
+    id: id,
+  };
+}
+
+function parsePacket(content) {
+  try {
+    return JSON.parse(content);
+  } catch (e) {
+    return content;
+  }
+}
+
+async function main() {
+  try {
+    client = new faye.Client(FAYE_URL);
+    console.log(
+      'Faye customer service starts...\n===============================',
+    );
+
+    client.subscribe('/get-customers_ack', getCustomers);
+    client.subscribe('/add-customer', addCustomer);
+  } catch (err) {
+    console.log('Error connecting to Faye: ', err.stack);
+  }
+}
+
+main();
